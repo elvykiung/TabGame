d596c3ad1c62065b8fe13af86223d8b533a17b13
Elvy Kiung <kiungh22@gmail.com>
2019-12-24 15:03:56 -0800

    add basic function

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [bonus, setBonus] = useState(1);
+  const [fertilizer, setFertilizer] = useState(9);
+
+  const clickHandler = () => {
+    setCount(count + bonus);
+  };
+
+  const handleBonus = () => {
+    setCount(count - fertilizer);
+    setBonus(bonus + 1);
+    setFertilizer(fertilizer + 10);
+  };
+  let pic = {
+    uri: "https://cb2.scene7.com/is/image/CB2/PottedMonsteraPlantSHS19/?$web_product_hero$&190410160828&wid=625&hei=625"
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Init</Text>
+      <Text style={styles.tabFontSize}>{count === 0 ? "Tap to start" : "$" + count} </Text>
+      <TouchableHighlight onPress={clickHandler}>
+        <Image source={pic} style={{ width: 193, height: 200 }} />
+      </TouchableHighlight>
+      <View>{count >= fertilizer ? <Button title={`Fertilize $ ${fertilizer}`} onPress={handleBonus} style={styles.tabFontSize} /> : <Text style={styles.tabFontSize}>Tap Tap Tap</Text>}</View>
     </View>
   );
 }
@@ -14,6 +35,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "space-around",
+    flexDirection: "column"
+  },
+  tabFontSize: {
+    fontSize: 30
   }
 });
